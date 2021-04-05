9b28ed71d3058cd7e0f99630d44f4fb6389fbe46
Anuranjan Bose <anuranjan.bose@tothenew.com>
2021-04-05 14:45:25 +0530

    Added SnapRefreshController, changes in AppDelegate

---
+++ SnapRefresh/SnapRefreshController.swift
@@ -0,0 +1,15 @@
+//
+//  SnapRefreshController.swift
+//  SnapRefresh
+//
+//  Created by Anuranjan Bose on 05/04/21.
+//
+
+import UIKit
+
+class SnapRefreshController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
