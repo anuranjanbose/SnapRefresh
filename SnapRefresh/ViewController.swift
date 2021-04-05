f251b897c022bd388a80e8953a13719d95122464
Anuranjan Bose <anuranjan.bose@tothenew.com>
2021-04-05 14:38:51 +0530

    Initial project setup

---
+++ SnapRefresh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapRefresh
+//
+//  Created by Anuranjan Bose on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
