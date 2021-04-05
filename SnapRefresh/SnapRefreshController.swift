7af1759dd5d0c85b49921ab72f9d516176dce4ca
Anuranjan Bose <anuranjan.bose@tothenew.com>
2021-04-05 15:06:33 +0530

    Fixed code for iOS 13+

--- SnapRefresh/SnapRefreshController.swift
+++ SnapRefresh/SnapRefreshController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class SnapRefreshController: UIViewController {
     
+    fileprivate let startingHeight: CGFloat = 50.0
+    fileprivate let shapeLayer: CAShapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        shapeLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: startingHeight)
+        shapeLayer.backgroundColor = UIColor.darkGray.cgColor
+        view.layer.addSublayer(shapeLayer)
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.userIsDragging)))
+    }
+    
+    @objc fileprivate func userIsDragging(gesture: UIPanGestureRecognizer) {
+        
+        let gestureState = gesture.state
+        if gestureState == .ended || gestureState == .failed || gestureState == .cancelled {
+            
+        } else {
+            shapeLayer.frame.size.height = startingHeight + gesture.translation(in: self.view).y
+        }
     }
 }
